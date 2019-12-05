531cbf26bf0d93c6df04ae6501f9ec2111ed6efb
Aly Mohamed Aly <mo7tarif1@hotmail.com>
2019-12-05 22:03:12 +0200

    added basic logic

--- app/src/main/java/com/example/cardmatch/MainActivity.java
+++ app/src/main/java/com/example/cardmatch/MainActivity.java
@@ -7,16 +7,16 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
+import java.util.HashMap;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageButton Cards[] = new ImageButton[8];
     final int BlurMe = Color.argb(255,50, 141, 168);
-    int[] Locations = {0,0,0,0,0,0,0,0};
+    HashMap<Integer,Integer> Location = new HashMap<Integer, Integer>();
+    ImageButton PressedCard = null;
     boolean Pressed = false;
-
-    public void PressMe(){ Pressed = (Pressed ? false : true); }
 
     public static int[] getRandomNumbers(){
         Random rand = new Random();
@@ -51,16 +51,36 @@ public class MainActivity extends AppCompatActivity {
         int[] ImageIds = {R.drawable.apple, R.drawable.coconut, R.drawable.orange, R.drawable.milk};
         for (int i = 0; i < 8; i++){
             Cards[RandomArray[i]].setImageResource(ImageIds[i/2]);
-            Locations[RandomArray[i]] = i/2;
+            Location.put(Cards[RandomArray[i]].getId(), i/2);
             Cards[RandomArray[i]].setColorFilter(BlurMe);
             Cards[RandomArray[i]].setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     ImageButton TempPhoto = (ImageButton) v;
-                    TempPhoto.clearColorFilter();
+                    Log.d("msg", "getResources: " + getResources().getResourceName(v.getId()));
+                    Log.d("msg", "TempPhoto: " + TempPhoto.getId());
+                    Log.d("msg", "onClick: " + TempPhoto.getDrawable().getCurrent());
+                    if (Pressed){
+                        if (TempPhoto.getColorFilter() != null){
+                            TempPhoto.clearColorFilter();
+                            if (Location.get(TempPhoto.getId()) == Location.get(PressedCard.getId())){
+                                Log.d("msg", "onClick: Here");
+                            }else{
+                                TempPhoto.setColorFilter(BlurMe);
+                                PressedCard.setColorFilter(BlurMe);
+                            }
+                            Pressed = false;
+                            PressedCard = null;
+                        }
+                    }else{
+                        if (TempPhoto.getColorFilter() != null){
+                            TempPhoto.clearColorFilter();
+                            PressedCard = TempPhoto;
+                            Pressed = true;
+                        }
+                    }
                 }
             });
         }
-
     }
 }
